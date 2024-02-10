4f0055d757dd17486dd8731466cdf3ceafb181bb
deebloo <dannybluedesign@gmail.com>
2024-02-09 20:59:38 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
-import { execSync } from "node:child_process";
+import path from "node:path";
+import { parseArgs } from "node:util";
 
-const pkgs = JSON.parse(
-  execSync("npm ls --all --json --lockfile-only").toString()
-);
+import { PackageAnalyzer } from "./package-analyzer.js";
 
-console.log(pkgs);
+const args = parseArgs({
+  options: {
+    root: {
+      type: "string",
+      default: "./",
+    },
+  },
+});
+
+const analyzer = new PackageAnalyzer(args.values.root);
+const importMap = new Map<string, string>();
+
+for await (let res of analyzer) {
+  if (res.config.module) {
+    importMap.set(res.config.name, path.join("/", res.path, res.config.module));
+  } else if (res.config.main) {
+    importMap.set(res.config.name, path.join("/", res.path, res.config.main));
+  }
+}
+
+const res: Record<string, string> = {};
+
+for (let [key, val] of importMap) {
+  res[key] = val;
+}
+
+console.log(res);
